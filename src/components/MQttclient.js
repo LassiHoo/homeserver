bdfb0cd79594e5c10718d0edaaca2d1f3fc54d86
Lassi Hirvonen <lassi@hirvonen.net>
2019-12-29 08:43:01 -0500

    3233ffee

--- src/components/MQttclient.js
+++ src/components/MQttclient.js
@@ -88,19 +88,7 @@ class MQTTtest extends Component{
            var object = JSON.parse(str);
            for (var u = 0; u < object["time"].length; u++) {
              const add = {time:object["time"][u],value:object["temp"][u]};
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-             // fffjkkkj
-=======
-             // fffhh
->>>>>>> testi4
->>>>>>> #
-=======
-             // fffhh
->>>>>>> te#
+            //ffddd444cccc
              console.log("Add  json value");
              console.log(add);
              testdata[i].data.push(add);
